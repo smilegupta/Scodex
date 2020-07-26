a4e93a5fdf1a1436ca3742cf468c412325a1772c
smilegupta <guptamiley3012@gmail.com>
2020-07-26 22:58:50 +0530

    Footer Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/HeaderPage";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
