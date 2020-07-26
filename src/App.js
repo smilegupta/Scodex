0eac784d0e8cb21c5782eda16075741f24f18eca
smilegupta <guptamiley3012@gmail.com>
2020-07-26 16:38:57 +0530

    initial setup of project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
      <h1> Learning React </h1> 
     </div>
   );
