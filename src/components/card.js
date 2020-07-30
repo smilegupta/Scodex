6d07bcd05db93f526f7dd12451121e2528360a37
smilegupta <guptamiley3012@gmail.com>
2020-07-31 02:30:27 +0530

    card fixed

--- src/components/card.js
+++ src/components/card.js
@@ -2,14 +2,30 @@ import React from "react";
 
 const card = () => {
   return (
-    <div>
-      <div class="card " style={{ borderRadius: "30px", boxShadow: "none", backgroundColor:"pink"}}>
-        <div class="card-content">
-          <p class="title has-text-white">
-            “There are two hard things in computer science: cache invalidation,
-            naming things, and off-by-one errors.”
-          </p>
-        </div>
+    <div
+      className="has-text-weight-semibold"
+      style={{
+        borderRadius: "30px",
+        backgroundColor: "white",
+        width: "220px",
+        padding: "10px",
+      }}
+    >
+      <div class="has-text-centered">
+        <img
+          src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/gfg_200X200.png"
+          alt="logo"
+          height="50px"
+          width="50px"
+        />
+        <p class="is-size-5">
+          {" "}
+          <a href="google" target="_blank" rel="noopener noreferrer">
+            {" "}
+            GeeksforGeeks{" "}
+          </a>
+        </p>
+        <br />
       </div>
     </div>
   );
